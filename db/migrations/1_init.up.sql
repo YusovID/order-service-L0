0296ed8b3077fafc1c821d55c42b385621a7242b
Ilya Yusov <yusovilya@gmail.com>
2025-08-15 16:13:41 +0300

    написал миграции для создания таблиц

---
+++ db/migrations/1_init.up.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS orders (
+    order_uid        UUID PRIMARY KEY,
+    track_number     TEXT NOT NULL,
+    customer_id      TEXT NOT NULL,
+    delivery_service TEXT NOT NULL,
+    date_created     TIMESTAMP WITH TIME ZONE NOT NULL,
+    payment_data     JSONB NOT NULL,
+    delivery_data    JSONB NOT NULL,
+    additional_data  JSONB NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    id           SERIAL PRIMARY KEY,
+    order_uid    UUID REFERENCES orders(order_uid) NOT NULL,
+    chrt_id      INTEGER NOT NULL,
+	track_number TEXT NOT NULL,
+	price        NUMERIC NOT NULL,
+	rid          TEXT NOT NULL,
+	name         TEXT NOT NULL,
+	sale         NUMERIC NOT NULL,
+	size         TEXT NOT NULL,
+	total_price  NUMERIC NOT NULL,
+	nm_id        INTEGER NOT NULL,
+	brand        TEXT NOT NULL,
+	status       INTEGER NOT NULL
+);
+
+CREATE UNIQUE INDEX customer_id_idx ON orders (customer_id);
