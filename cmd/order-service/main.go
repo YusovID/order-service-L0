4701fd8c919be9bebb00a75ac18e1650923bffb2
Ilya Yusov <yusovilya@gmail.com>
2025-08-15 16:14:52 +0300

    добавил подключение к базе данных в сервис

--- cmd/order-service/main.go
+++ cmd/order-service/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log"
 	"log/slog"
+	"os"
 
 	"github.com/YusovID/order-service/internal/config"
+	"github.com/YusovID/order-service/internal/storage/postgres"
+	"github.com/YusovID/order-service/lib/logger/sl"
 	"github.com/YusovID/order-service/lib/logger/slogpretty"
 	"github.com/joho/godotenv"
 )
@@ -23,11 +26,19 @@ func main() {
 
 	log.Info("starting order service", slog.String("env", cfg.Env))
 
-	// TODO реализовать сервер
+	storage, err := postgres.New(cfg.Postgres, log)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	log.Info("storage initialization successful")
+
+	_ = storage
+
+	// TODO добавить кэш
 
 	// TODO создать подключение к Kafka
 
-	// TODO подключить PostgreSQL
-
-	// TODO добавить кэш
+	// TODO реализовать сервер
 }
