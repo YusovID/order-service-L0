942d8d7afab77d10a6399837be991c4a50672c61
Ilya Yusov <yusovilya@gmail.com>
2025-08-24 16:58:57 +0300

    обновил конфиг

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,24 +8,47 @@ import (
 )
 
 type Config struct {
-	Env      string   `yaml:"env" env-required:"true"`
+	Env      string   `yaml:"env" env:"ENV" env-required:"true"`
 	Postgres Postgres `yaml:"postgres" env-required:"true"`
 	Redis    Redis    `yaml:"redis" env-required:"true"`
+	Kafka    Kafka    `yaml:"kafka" env-required:"true"`
 }
 
 type Postgres struct {
-	Username string `yaml:"username" env-required:"true"`
-	Password string `yaml:"password" env-required:"true"`
-	Host     string `yaml:"host" env-required:"true"`
-	Port     string `yaml:"port" env-required:"true"`
-	Database string `yaml:"database" env-required:"true"`
+	Username string `yaml:"username" env:"POSTGRES_USER" env-required:"true"`
+	Password string `yaml:"password" env:"POSTGRES_PASSWORD" env-required:"true"`
+	Host     string `yaml:"host" env:"POSTGRES_HOST" env-required:"true"`
+	Port     string `yaml:"port" env:"POSTGRES_PORT" env-required:"true"`
+	Database string `yaml:"database" env:"POSTGRES_DB" env-required:"true"`
 }
 
 type Redis struct {
-	Host     string `yaml:"host" env-required:"true"`
-	Port     string `yaml:"port" env-required:"true"`
-	DB       int    `yaml:"database"`
-	Password string `yaml:"password"`
+	Host     string `yaml:"host" env:"REDIS_HOST" env-required:"true"`
+	Port     string `yaml:"port" env:"REDIS_PORT" env-required:"true"`
+	DB       int    `yaml:"database" env:"REDIS_DB"`
+	Password string `yaml:"password" env:"REDIS_PASSWORD"`
+}
+
+type Kafka struct {
+	BootstrapServers []string `yaml:"bootstrap.servers" env:"KAFKA_BOOTSTRAP_SERVERS" env-required:"true"`
+	Topic            string   `yaml:"topic" env-required:"true"`
+	Producer         Producer `yaml:"producer" env-required:"true"`
+	Consumer         Consumer `yaml:"consumer" env-required:"true"`
+}
+
+type Producer struct {
+	Acks              int    `yaml:"acks" env-required:"true"`
+	EnableIdempotence bool   `yaml:"enable.idempotence"`
+	Retries           int    `yaml:"retries"`
+	TransactionalId   string `yaml:"transactional.id"`
+}
+
+type Consumer struct {
+	GroupId          string `yaml:"group.id" env-required:"true"`
+	AutoOffsetReset  string `yaml:"auto.offset.reset" env-required:"true"`
+	EnableAutoCommit bool   `yaml:"enable.auto.commit"`
+	SecurityProtocol string `yaml:"security.protocol"`
+	IsolationLevel   int8   `yaml:"isolation.level"`
 }
 
 func MustLoad() *Config {
