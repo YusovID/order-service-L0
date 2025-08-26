94c7ce669c453bd3054904d6d3b6af37f232033b
Ilya Yusov <yusovilya@gmail.com>
2025-08-26 14:57:44 +0300

    добавил роутер и сервер

--- cmd/order-service/main.go
+++ cmd/order-service/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"context"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 
 	"github.com/YusovID/order-service/internal/config"
+	"github.com/YusovID/order-service/internal/http-server/handlers/url/get"
+	mwLogger "github.com/YusovID/order-service/internal/http-server/middleware/logger"
 	"github.com/YusovID/order-service/internal/storage/kafka"
 	"github.com/YusovID/order-service/internal/storage/postgres"
 	"github.com/YusovID/order-service/internal/storage/redis"
 	"github.com/YusovID/order-service/lib/logger/sl"
 	"github.com/YusovID/order-service/lib/logger/slogpretty"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -29,6 +34,7 @@ func main() {
 	storage, err := postgres.New(cfg.Postgres, log)
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
+
 		os.Exit(1)
 	}
 
@@ -39,9 +45,10 @@ func main() {
 	wg.Add(1)
 	go storage.ProcessOrder(ctx, orderChan, wg)
 
-	_, err = redis.New(ctx, cfg.Redis)
+	cache, err := redis.New(ctx, cfg.Redis)
 	if err != nil {
 		log.Error("failed to init cache", sl.Err(err))
+
 		os.Exit(1)
 	}
 
@@ -50,6 +57,7 @@ func main() {
 	c, err := kafka.NewConsumer(cfg.Kafka, log)
 	if err != nil {
 		log.Error("failed to init consumer", sl.Err(err))
+
 		os.Exit(1)
 	}
 
@@ -63,13 +71,52 @@ func main() {
 	wg.Add(1)
 	go c.ProcessMessages(ctx, cfg.Kafka.Topic, orderChan, wg)
 
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Get("/order/{order_uid}", get.New(ctx, log, cache, storage))
+
+	log.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error("failed to start server", sl.Err(err))
+
+			os.Exit(1)
+		}
+	}()
+
 	<-sigchan
 	cancel()
 
 	wg.Wait()
 
 	log.Info("shutting down consumer")
-	c.Consumer.Close()
+	if err = c.Consumer.Close(); err != nil {
+		slog.Error("failed to close consumer", sl.Err(err))
 
-	// TODO реализовать сервер
+		os.Exit(1)
+	}
+
+	log.Info("stopping server")
+
+	err = srv.Shutdown(ctx)
+	if err != nil {
+		log.Error("failed to shutdown server", sl.Err(err))
+
+		os.Exit(1)
+	}
 }
