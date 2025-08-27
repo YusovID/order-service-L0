421c5dd8ff7ca90a4120d4cd82a1327c2c84c5dd
Ilya Yusov <yusovilya@gmail.com>
2025-08-27 16:38:22 +0300

    вынес AdditionalData в отельную структуру

--- internal/models/order_data.go
+++ internal/models/order_data.go
@@ -3,20 +3,17 @@ package models
 import "time"
 
 type OrderData struct {
-	OrderUID          string    `json:"order_uid"`
-	TrackNumber       string    `json:"track_number"`
-	Entry             string    `json:"entry"`
-	Delivery          Delivery  `json:"delivery"`
-	Payment           Payment   `json:"payment"`
-	Items             []Item    `json:"items"`
-	Locale            string    `json:"locale"`
-	InternalSignature string    `json:"internal_signature"`
-	CustomerID        string    `json:"customer_id"`
-	DeliveryService   string    `json:"delivery_service"`
-	Shardkey          string    `json:"shardkey"`
-	SmID              int       `json:"sm_id"`
-	DateCreated       time.Time `json:"date_created"`
-	OofShard          string    `json:"oof_shard"`
+	OrderUID        string    `json:"order_uid"`
+	TrackNumber     string    `json:"track_number"`
+	CustomerID      string    `json:"customer_id"`
+	DeliveryService string    `json:"delivery_service"`
+	DateCreated     time.Time `json:"date_created"`
+
+	Items []Item `json:"items"`
+
+	Delivery   Delivery `json:"delivery"`
+	Payment    Payment  `json:"payment"`
+	AdditionalData
 }
 
 type Delivery struct {
@@ -40,6 +37,15 @@ type Payment struct {
 	DeliveryCost int    `json:"delivery_cost"`
 	GoodsTotal   int    `json:"goods_total"`
 	CustomFee    int    `json:"custom_fee"`
+}
+
+type AdditionalData struct {
+	Entry             string `json:"entry"`
+	Locale            string `json:"locale"`
+	InternalSignature string `json:"internal_signature"`
+	Shardkey          string `json:"shardkey"`
+	SmID              int    `json:"sm_id"`
+	OofShard          string `json:"oof_shard"`
 }
 
 type Item struct {
