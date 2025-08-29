5e73c648c8b842fca6a5dcf148f4b0186714b072
Ilya Yusov <yusovilya@gmail.com>
2025-08-29 16:31:22 +0300

    перешел на библиотеку gofakeit в generator

--- lib/generator/order/order.go
+++ lib/generator/order/order.go
@@ -3,115 +3,99 @@ package orderGen
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand/v2"
-	"time"
 
 	"github.com/YusovID/order-service/internal/models"
-	"github.com/google/uuid"
+	"github.com/brianvoe/gofakeit/v7"
 )
 
 var (
 	deliveryServices = []string{"meest", "dhl", "fedex", "cdek"}
 	providers        = []string{"wbpay", "payu", "stripe", "visa", "mastercard"}
 	banks            = []string{"alpha", "sber", "vtb", "tinkoff"}
-	firstNames       = []string{"Иван", "Мария", "Алексей", "Елена", "Дмитрий"}
-	lastNames        = []string{"Иванов", "Петрова", "Смирнов", "Соколова", "Кузнецов"}
-	cities           = []string{"Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Казань"}
-	brands           = []string{"Vivienne Sabo", "Nike", "Adidas", "H&M", "ZARA"}
-	itemNames        = []string{"Mascaras", "T-shirt", "Jeans", "Sneakers", "Dress"}
 )
 
 func GenerateOrder() []byte {
-	orderUID := uuid.New().String()
-	customerUID := uuid.New().String()
-	transactionID := orderUID
-	dateCreated := time.Now().UTC()
+	orderUID := gofakeit.UUID()
+	trackNumber := gofakeit.LetterN(4) + gofakeit.DigitN(8)
+	dateCreated := gofakeit.Date()
 
-	itemsCount := randomInt(1, 3)
+	itemsCount := gofakeit.Number(1, 3)
 	items := make([]models.Item, itemsCount)
-	goodsTotal := 0.0
+	var goodsTotal int
 	for i := 0; i < itemsCount; i++ {
-		item := generateItem()
+		item := generateItem(trackNumber)
 		items[i] = item
-		goodsTotal += item.TotalPrice
+		goodsTotal += int(item.TotalPrice)
 	}
-
-	randomFirstName := randomChoice(firstNames)
-	randomLastName := randomChoice(lastNames)
 
 	delivery := models.Delivery{
-		Name:    fmt.Sprintf("%s %s", randomFirstName, randomLastName),
-		Phone:   "+972" + fmt.Sprint(randomInt(100000000, 999999999)),
-		Zip:     fmt.Sprint(randomInt(1000000, 9999999)),
-		City:    randomChoice(cities),
-		Address: "Random street " + fmt.Sprint(randomInt(1, 100)),
-		Region:  "Kraiot",
-		Email:   fmt.Sprintf("%s%s@test.com", randomFirstName, randomLastName),
+		Name:    gofakeit.Name(),
+		Phone:   gofakeit.Phone(),
+		Zip:     gofakeit.Zip(),
+		City:    gofakeit.City(),
+		Address: gofakeit.Address().Address,
+		Region:  gofakeit.State(),
+		Email:   gofakeit.Email(),
 	}
 
-	deliveryCost := float64(randomInt(500, 2500))
+	deliveryCost := gofakeit.Number(500, 2500)
 	payment := models.Payment{
-		Transaction:  transactionID,
+		Transaction:  orderUID,
 		RequestID:    "",
 		Currency:     "USD",
-		Provider:     randomChoice(providers),
-		Amount:       int(goodsTotal + deliveryCost),
+		Provider:     gofakeit.RandomString(providers),
+		Amount:       goodsTotal + deliveryCost,
 		PaymentDT:    int(dateCreated.Unix()),
-		Bank:         randomChoice(banks),
-		DeliveryCost: int(deliveryCost),
-		GoodsTotal:   int(goodsTotal),
+		Bank:         gofakeit.RandomString(banks),
+		DeliveryCost: deliveryCost,
+		GoodsTotal:   goodsTotal,
 		CustomFee:    0,
 	}
 
 	order := models.OrderData{
-		OrderUID:          orderUID,
-		TrackNumber:       "WBILMTESTTRACK",
-		Entry:             "WBIL",
-		Delivery:          delivery,
-		Payment:           payment,
-		Items:             items,
-		Locale:            "en",
-		InternalSignature: "",
-		CustomerID:        customerUID,
-		DeliveryService:   randomChoice(deliveryServices),
-		Shardkey:          fmt.Sprint(randomInt(1, 9)),
-		SmID:              randomInt(1, 100),
-		DateCreated:       dateCreated,
-		OofShard:          "1",
+		OrderUID:        orderUID,
+		TrackNumber:     trackNumber,
+		CustomerID:      gofakeit.UUID(),
+		DeliveryService: gofakeit.RandomString(deliveryServices),
+		DateCreated:     dateCreated,
+		Items:           items,
+		Delivery:        delivery,
+		Payment:         payment,
+		AdditionalData: models.AdditionalData{
+			Entry:             "WBIL",
+			Locale:            gofakeit.LanguageAbbreviation(),
+			InternalSignature: "",
+			Shardkey:          gofakeit.Digit(),
+			SmID:              gofakeit.Number(1, 100),
+			OofShard:          "1",
+		},
 	}
 
 	jsonData, err := json.Marshal(order)
 	if err != nil {
+		fmt.Println("Error marshaling to JSON:", err)
 		return nil
 	}
 
 	return jsonData
 }
 
-func randomChoice(slice []string) string {
-	return slice[rand.IntN(len(slice))]
-}
-
-func randomInt(min, max int) int {
-	return rand.IntN(max-min+1) + min
-}
-
-func generateItem() models.Item {
-	price := randomInt(100, 1000)
-	sale := randomInt(0, 50)
-	totalPrice := price - (price*sale)/100
+func generateItem(trackNumber string) models.Item {
+	price := gofakeit.Price(100, 1000)
+	sale := gofakeit.Number(0, 50)
+	totalPrice := price - (price*float64(sale))/100
 
 	return models.Item{
-		ChrtID:      randomInt(1000000, 9999999),
-		TrackNumber: "WBILMTESTTRACK",
-		Price:       float64(price),
-		Rid:         uuid.New().String(),
-		Name:        randomChoice(itemNames),
+		ChrtID:      gofakeit.Number(1000000, 9999999),
+		TrackNumber: trackNumber,
+		Price:       price,
+		Rid:         gofakeit.UUID(),
+		Name:        gofakeit.ProductName(),
 		Sale:        float64(sale),
-		Size:        "0",
-		TotalPrice:  float64(totalPrice),
-		NmID:        randomInt(1000000, 9999999),
-		Brand:       randomChoice(brands),
+		Size:        gofakeit.RandomString([]string{"S", "M", "L", "XL"}),
+		TotalPrice:  totalPrice,
+		NmID:        gofakeit.Number(1000000, 9999999),
+		Brand:       gofakeit.Company(),
 		Status:      202,
 	}
 }
